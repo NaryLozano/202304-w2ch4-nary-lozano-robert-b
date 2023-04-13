41651c52c57153cbaee8b6edbc8a705b9a1e16e3
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-04-13 22:02:09 +0200

    Delete sneaky console.log

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import CodersArray from "./CodersArray/CodersArray.js";
+
+const names = new CodersArray("hola", "nary", "holaa", "errerer", "asdf");
+console.log(names.length());
+console.log(names.push("robert", "nary"));
+console.log(names);
